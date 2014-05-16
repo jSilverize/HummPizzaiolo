369b6c6185730457be3a24b654fd28361659d2ba
Udimberto Silvestre Junior <elvitous@gmail.com>
2014-05-15 22:27:50 -0300

    Update HummPizzaiolo.java

--- HummPizzaiolo/src/experimentos/HummPizzaiolo.java
+++ HummPizzaiolo/src/experimentos/HummPizzaiolo.java
@@ -1,5 +1,6 @@
 package experimentos;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /*
@@ -7,168 +8,118 @@ import java.util.Scanner;
  */
 public class HummPizzaiolo {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-    exibirMenu();
+        Item[] listaItens = new Item[50];
 
-  }
+        int listaOpcoes;
 
-  static Scanner leia = new Scanner(System.in);
+        do {
 
-  static void exibirMenu() {
+            System.out.println("\n -------- HUMM PIZZAIOLO -------- ");
+            System.out.println("\n\t  MENU PRINCIPAL");
 
-    int listaOpcoes;
+            System.out.println("\n1 - PEDIDOS\n2 - CLIENTES\n3 - ITENS\n\n0 - FINALIZAR PROGRAMA\n");
 
-    do {
+            System.out.print("Digite uma das opções: ");
+            listaOpcoes = leia.nextInt();
 
-      System.out.println("\n -------- HUMM PIZZAIOLO -------- ");
-      System.out.println("\n\t  MENU PRINCIPAL");
+            switch (listaOpcoes) {
+                case 0:
+                    break;
+                case 1:
+                    pedidosMenu();
+                    break;
+                case 2:
+                    clientesMenu();
+                    break;
+                case 3:
+                    listaItens = Item.menu(listaItens);
+                    break;
+                default:
+                    System.out.println("Favor digitar uma opcao valida.\n");
+                    listaOpcoes = 1;
+                    break;
+            }
 
-      System.out.println("\n1 - PEDIDOS\n2 - CLIENTES\n3 - ITENS\n\n0 - FINALIZAR PROGRAMA\n");
+        } while (listaOpcoes > 0);
 
-      System.out.print("Digite uma das opções: ");
-      listaOpcoes = leia.nextInt();
+    }
 
-      switch (listaOpcoes) {
-        case 0:
-          break;
-        case 1:
-          pedidosMenu();
-          break;
-        case 2:
-          clientesMenu();
-          break;
-        case 3:
-          itensMenu();
-          break;
-        default:
-          System.out.println("Favor digitar uma opcao valida.\n");
-          listaOpcoes = 1;
-          break;
-      }
+    static Scanner leia = new Scanner(System.in);
 
-    } while (listaOpcoes > 0);
+    static void pedidosMenu() {
 
-  }
+        System.out.println("EM CONSTRUCAO\n\n");
 
-  static void pedidosMenu() {
+        int listaOpcoes;
 
-    System.out.println("EM CONSTRUCAO\n\n");
+        do {
+            System.out.println("\n -------- HUMM PIZZAIOLO -------- ");
+            System.out.println("\n\t     PEDIDOS\n");
 
-    int listaOpcoes;
+            System.out.println("1 - BUSCAR\n2 - CADASTRAR\n3 - ALTERAR\n4 - EXCLUIR\n\n0 - MENU PRINCIPAL");
+            System.out.print("\nDigite uma das opções: ");
+            listaOpcoes = leia.nextInt();
+            switch (listaOpcoes) {
+                case 0:
+                    // VOLTAR para o MENU PRINCIPAL
+                    break;
+                case 1:
+                    // buscarPedido();
+                    break;
+                case 2:
+                    // cadastrarPedido();
+                    break;
+                case 3:
+                    // alterarPedido();
+                    break;
+                case 4:
+                    // excluirPedido();
+                    break;
+                default:
+                    System.out.println("Favor digitar uma opcao valida.\n");
+                    listaOpcoes = 0;
+                    break;
+            }
+        } while (listaOpcoes > 0);
 
-    do {
-      System.out.println("\n -------- HUMM PIZZAIOLO -------- ");
-      System.out.println("\n\t     PEDIDOS\n");
+    }
 
-      System.out.println("1 - BUSCAR\n2 - CADASTRAR\n3 - ALTERAR\n4 - EXCLUIR\n\n0 - MENU PRINCIPAL");
-      System.out.print("\nDigite uma das opções: ");
-      listaOpcoes = leia.nextInt();
-      switch (listaOpcoes) {
-        case 0:
-          // VOLTAR para o MENU PRINCIPAL
-          break;
-        case 1:
-          // buscarPedido();
-          break;
-        case 2:
-          // cadastrarPedido();
-          break;
-        case 3:
-          // alterarPedido();
-          break;
-        case 4:
-          // excluirPedido();
-          break;
-        default:
-          System.out.println("Favor digitar uma opcao valida.\n");
-          listaOpcoes = 0;
-          break;
-      }
-    } while (listaOpcoes > 0);
+    static void clientesMenu() {
+        System.out.println("EM CONSTRUCAO\n\n");
 
-  }
+        int listaOpcoes;
 
-  static void clientesMenu() {
-    System.out.println("EM CONSTRUCAO\n\n");
+        do {
+            System.out.println("\n -------- HUMM PIZZAIOLO -------- ");
+            System.out.println("\n\t      CLIENTES\n");
 
-    int listaOpcoes;
-
-    do {
-      System.out.println("\n -------- HUMM PIZZAIOLO -------- ");
-      System.out.println("\n\t      CLIENTES\n");
-
-      System.out.println("1 - BUSCAR\n2 - CADASTRAR\n3 - ALTERAR\n4 - EXCLUIR\n\n0 - MENU PRINCIPAL");
-      System.out.print("\nDigite uma das opções: ");
-      listaOpcoes = leia.nextInt();
-      switch (listaOpcoes) {
-        case 0:
-          // VOLTAR para o MENU PRINCIPAL
-          break;
-        case 1:
-          // buscarCliente();
-          break;
-        case 2:
-          // cadastrarCliente();
-          break;
-        case 3:
-          // alterarCliente();
-          break;
-        case 4:
-          // excluirCliente();
-          break;
-        default:
-          System.out.println("Favor digitar uma opcao valida.\n");
-          listaOpcoes = 0;
-          break;
-      }
-    } while (listaOpcoes > 0);
-  }
-
-  static void itensMenu() {
-
-    Item[] listaItens = new Item[50];
-
-    int listaOpcoes;
-    int ultimo = 0;
-
-    do {
-      System.out.println("\n -------- HUMM PIZZAIOLO -------- ");
-      System.out.println("\n\t       ITENS\n");
-
-      System.out.println("1 - BUSCAR\n2 - CADASTRAR\n3 - ALTERAR\n4 - EXCLUIR\n\n0 - MENU PRINCIPAL");
-      System.out.print("\nDigite uma das opções: ");
-      listaOpcoes = leia.nextInt();
-      switch (listaOpcoes) {
-        case 0:
-          // VOLTAR para o MENU PRINCIPAL
-          break;
-        case 1:
-          Item.buscar(listaItens);
-          break;
-        case 2:
-          Item it = Item.lerDados();
-          Item.cadastrar(listaItens, it);
-          break;
-        case 3:
-          int pos = Item.buscar(listaItens);
-          if (pos > -1) {
-            Item.alterar(listaItens, ultimo, pos);
-            System.out.println("Item Alterado!");
-          } else {
-            System.out.println("Item Inexistente!");
-          }
-          break;
-        case 4:
-          // excluirItem();
-          break;
-        default:
-          System.out.println("Favor digitar uma opcao valida.\n");
-          listaOpcoes = 0;
-          break;
-      }
-    } while (listaOpcoes > 0);
-
-  }
+            System.out.println("1 - BUSCAR\n2 - CADASTRAR\n3 - ALTERAR\n4 - EXCLUIR\n\n0 - MENU PRINCIPAL");
+            System.out.print("\nDigite uma das opções: ");
+            listaOpcoes = leia.nextInt();
+            switch (listaOpcoes) {
+                case 0:
+                    // VOLTAR para o MENU PRINCIPAL
+                    break;
+                case 1:
+                    // buscarCliente();
+                    break;
+                case 2:
+                    // cadastrarCliente();
+                    break;
+                case 3:
+                    // alterarCliente();
+                    break;
+                case 4:
+                    // excluirCliente();
+                    break;
+                default:
+                    System.out.println("Favor digitar uma opcao valida.\n");
+                    listaOpcoes = 0;
+                    break;
+            }
+        } while (listaOpcoes > 0);
+    }
 
 }
