195f7f4856d2641f218f28ec107653dcbfbf7af7
jsilverize <jsilverize@Dev01>
2014-05-29 14:42:52 -0300

    Modificações incompletas.

--- HummPizzaiolo/src/br/com/hummpizzaiolo/view/Menu.java
+++ HummPizzaiolo/src/br/com/hummpizzaiolo/view/Menu.java
@@ -1,84 +1,74 @@
 package br.com.hummpizzaiolo.view;
 
-/**
- *
- * @author marcos.vcnmarcos
- */
+import br.com.hummpizzaiolo.HummPizzaiolo;
+import static br.com.hummpizzaiolo.HummPizzaiolo.leia;
+import br.com.hummpizzaiolo.model.Item;
 import java.util.Scanner;
 
+/*
+ * @author jsilverize & markaotribe
+ */
 public class Menu {
 
-    static Scanner leia = new Scanner(System.in);
+  static Scanner leia = new Scanner(System.in);
 
-   public static void exibir() {
+  public static void exibir() {
 
-        int listaOpcoes;
+    int listaOpcoes;
 
-        do {
+    do {
 
-            System.out.print(" --- HUMM PIZZAIOLO --- ");
+      System.out.println("\n -------- HUMM PIZZAIOLO -------- ");
+      System.out.println("\n\t  MENU PRINCIPAL");
 
-            System.out.print("\n1 - PEDIDOS\n2 - CLIENTES\n3 - ITENS\n4 - FINALIZAR PROGRAMA\n");
-            System.out.print("\nDigite uma das opções acima ↑: \n");
+      System.out.println("\n1 - PEDIDOS\n2 - CLIENTES\n3 - ITENS\n\n0 - FINALIZAR PROGRAMA\n");
 
-            listaOpcoes = leia.nextInt();
+      System.out.print("Digite uma das opções: ");
+      listaOpcoes = leia.nextInt();
 
-            switch (listaOpcoes) {
-                case 1:
-                    Menu.pedidosMenu();
-                    int pedidos;
-                    pedidos = leia.nextInt();
-                    switch (pedidos){
-                        case 0:
-                            pedidos = 0;
-                        case 1:
-                            //FUNÇÃO CADASTRAR
-                            break;
-                        case 2:
-                            //FUNÇÃO ALTERAR
-                            break;
-                        case 3:
-                            //FUNÇÃO EXCLUIR
-                            break;
-                        default:
-                            System.out.print("OPÇÃO INEXISTENTE\n");
-                            listaOpcoes = 0;
-                    break;
-                    }
-                    break;
-                case 2:
-                    Menu.clientesMenu();
-                    break;
-                case 3:
-                    Menu.itensMenu();
-                    break;
-                default:
-                    System.out.print("OPÇÃO INEXISTENTE\n");
-                    listaOpcoes = 0;
-                    break;
-            }
+      switch (listaOpcoes) {
+        case 0:
+          break;
+        case 1:
+          pedidosMenu();
+          break;
+        case 2:
+          clientesMenu();
+          break;
+        case 3:
+          Item.menu();
+          break;
+        default:
+          System.out.println("Favor digitar uma opcao valida.\n");
+          listaOpcoes = 1;
+          break;
+      }
 
-        } while (listaOpcoes > 0);
-    }
-            //FUNÇÕES 
-    static void pedidosMenu() {
-        System.out.print(" --- PEDIDOS --- ");
-        Menu.miniMenu();
-        int listaOpcoes = leia.nextInt();
-    }
-    static void clientesMenu() {
-        System.out.print(" --- CLIENTES --- ");
-        Menu.miniMenu();
-        int listaOpcoes = leia.nextInt();
-    }
-    static void itensMenu() {
-        System.out.print(" --- ITENS --- ");
-        Menu.miniMenu();
-        int listaOpcoes = leia.nextInt();
-    }
-    static void miniMenu(){
-         System.out.print("\n1 - CADASTRAR\n2 - ALTERAR\n3 - EXCLUIR\n\n0 - RETORNAR AO MENU PRINCIPAL");
-         System.out.print("\nDigite uma das opções acima ↑: \n");
-    }
+    } while (listaOpcoes > 0);
+  }
+
+  //FUNÇÕES 
+  static void pedidosMenu() {
+    System.out.print(" --- PEDIDOS --- ");
+    Menu.crudMenu();
+    int listaOpcoes = leia.nextInt();
+  }
+
+  static void clientesMenu() {
+    System.out.print(" --- CLIENTES --- ");
+    Menu.crudMenu();
+    int listaOpcoes = leia.nextInt();
+  }
+
+  static void itensMenu() {
+    System.out.print(" --- ITENS --- ");
+    Menu.crudMenu();
+    int listaOpcoes = leia.nextInt();
+  }
+
+  static int crudMenu() {
+    System.out.println("1 - CADASTRAR\n2 - ALTERAR\n3 - EXCLUIR\n4 - PROCURA DE ITENS\n\n0 - MENU PRINCIPAL");
+    System.out.print("\nDigite uma das opções: ");
+    return HummPizzaiolo.leia.nextInt();
+  }
 }
-
